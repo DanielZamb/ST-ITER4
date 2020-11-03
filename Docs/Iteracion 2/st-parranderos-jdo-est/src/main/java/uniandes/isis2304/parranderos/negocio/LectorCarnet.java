3f2b92f41e713679cda28114862aef2d149e66a2
Felipe Rincón <af.rincon@uniandes.edu.co>
2020-11-02 20:03:53 -0500

    clases negocio

--- Docs/Iteracion 2/st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/negocio/LectorCarnet.java
+++ Docs/Iteracion 2/st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/negocio/LectorCarnet.java
@@ -2,4 +2,74 @@ package uniandes.isis2304.parranderos.negocio;
 
 public class LectorCarnet {
 
+	private long idLector;
+	
+	private long idEspacio;
+	
+	private long idVisitante;
+	
+	private String horaIngreso;
+	
+	private String horaSalida;
+	
+	public LectorCarnet()
+	{
+		this.idLector = 0;
+		this.idEspacio = 0;
+		this.idVisitante = 0;
+		this.horaIngreso = "";
+		this.horaSalida = "";	
+	}
+	
+	public LectorCarnet(long lector, long espacio, long visitante, String ingreso, String salida)
+	{
+		this.idLector = lector;
+		this.idEspacio = espacio;
+		this.idVisitante = visitante;
+		this.horaIngreso = ingreso;
+		this.horaSalida = salida;	
+	}
+
+	public long getIdLector() {
+		return idLector;
+	}
+
+	public void setIdLector(long idLector) {
+		this.idLector = idLector;
+	}
+
+	public long getIdEspacio() {
+		return idEspacio;
+	}
+
+	public void setIdEspacio(long idEspacio) {
+		this.idEspacio = idEspacio;
+	}
+
+	public long getIdVisitante() {
+		return idVisitante;
+	}
+
+	public void setIdVisitante(long idVisitante) {
+		this.idVisitante = idVisitante;
+	}
+
+	public String getHoraIngreso() {
+		return horaIngreso;
+	}
+
+	public void setHoraIngreso(String horaIngreso) {
+		this.horaIngreso = horaIngreso;
+	}
+
+	public String getHoraSalida() {
+		return horaSalida;
+	}
+
+	public void setHoraSalida(String horaSalida) {
+		this.horaSalida = horaSalida;
+	}
+	
+	
+	
 }
--- Docs/Iteracion 2/st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/negocio/Visitante.java
+++ Docs/Iteracion 2/st-parranderos-jdo-est/src/main/java/uniandes/isis2304/parranderos/negocio/Visitante.java
@@ -1,5 +1,99 @@
 package uniandes.isis2304.parranderos.negocio;
 
 public class Visitante {
+	
+	private long id;
+	
+	private String tipoId;
+	
+	private String nombre;
+	
+	private String correo;
+	
+	private int telefono;
+	
+	private String nombreContacto;
+	
+	private int telefonoContacto;
+	
+	private String tipoVisitante;
+	
+	public Visitante(long id, String tipoId, String nombre, String correo, int telefono, String nomC, int telC, String tipoV )
+	{
+		this.id = id;
+		this.tipoId = tipoId;
+		this.nombre = nombre;
+		this.correo = correo;
+		this.telefono = telefono;
+		this.nombreContacto = nomC;
+		this.telefonoContacto = telC;
+		this.tipoVisitante = tipoV;
+	}
 
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipoId() {
+		return tipoId;
+	}
+
+	public void setTipoId(String tipoId) {
+		this.tipoId = tipoId;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public int getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getNombreContacto() {
+		return nombreContacto;
+	}
+
+	public void setNombreContacto(String nombreContacto) {
+		this.nombreContacto = nombreContacto;
+	}
+
+	public int getTelefonoContacto() {
+		return telefonoContacto;
+	}
+
+	public void setTelefonoContacto(int telefonoContacto) {
+		this.telefonoContacto = telefonoContacto;
+	}
+
+	public String getTipoVisitante() {
+		return tipoVisitante;
+	}
+
+	public void setTipoVisitante(String tipoVisitante) {
+		this.tipoVisitante = tipoVisitante;
+	}
+	
+	
+	
 }
